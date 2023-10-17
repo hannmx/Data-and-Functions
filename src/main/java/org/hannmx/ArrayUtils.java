b45fff87714d57ac31b0e4464cc00d883c32c965
hannmx <mxkud@mail.ru>
2023-10-17 19:08:45 +0300

    Changed classes Main and ArrayUtils

--- src/main/java/org/hannmx/ArrayUtils.java
+++ src/main/java/org/hannmx/ArrayUtils.java
@@ -1,13 +1,36 @@
 package org.hannmx;
 
 public class ArrayUtils {   //реализация всех методов массива
-    public static int countEvent(int[] nums){
-        return 0;
+    public static int countEvent(int[] nums) {
+        int count = 0;
+        for (int num : nums) {
+            if (num % 2 == 0) {
+                count++;
+            }
+        }
+        return count;
     }
-    public static int findDifference(int[] nums){
-        return 0;
+
+    public static int findDifference(int[] nums) {
+        int min = nums[0];
+        int max = nums[0];
+        for (int num : nums) {
+            if (min > num) {
+                min = num;
+            }
+            if (max < num) {
+                max = num;
+            }
+        }
+        return max - min;
     }
-    public static boolean hasZeroes(int[] nums){
-        return true;
+
+    public static boolean hasZeroes(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0 && nums[i+1] == 0) {
+                return true;
+            }
+        }
+        return false;
     }
 }
--- src/main/java/org/hannmx/Main.java
+++ src/main/java/org/hannmx/Main.java
@@ -1,7 +1,19 @@
 package org.hannmx;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите элементы массива через пробел: ");
+        String[] input = scanner.nextLine().split(" ");
 
+        int[] array = new int[input.length];
+        for (int i = 0; i < input.length; i++) {
+            array[i] = Integer.parseInt(input[i]);
+        }
+        System.out.println("Количество чётных элементов массива: " + ArrayUtils.countEvent(array));
+        System.out.println("Разница между самым большим и самым маленьким элементами массива: " + ArrayUtils.findDifference(array));
+        System.out.println("В переданном массиве есть два соседних элемента, с нулевым значением: " + ArrayUtils.hasZeroes(array));
     }
 }
